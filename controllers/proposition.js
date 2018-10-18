020c911583bf33408d87a85ab4aa24a582fd637c
Mickael Moreira <dopidot@gmail.com>
2018-10-18 15:05:30 +0200

    Add proposition controller and router

---
+++ controllers/proposition.js
@@ -0,0 +1,139 @@
+const ModelIndex = require('../models');
+const Proposition = ModelIndex.Proposition;
+
+const PropositionController = function() {};
+
+PropositionController.addProposition = function(firstname, lastname, phone, email, photo, address, password, id_categ){
+  return User.create({
+    firstname: firstname,
+    lastname: lastname,
+    phone: phone,
+    email: email,
+    photo: photo,
+    address: address,
+    password: password,
+    category_id: id_categ
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+};
+
+PropositionController.deleteProposition = function(idUser){
+  return User.destroy({
+    where:{
+      id: idUser
+    }
+  })
+  .then(() => {
+      console.log("User was deleted.");
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+};
+
+
+PropositionController.updateProposition = function(idUser, newFirstname, newLastname, newPhone, newEmail, newPhoto, newAddress, newPassword, newId_categ) {
+  return User.find({
+    where:{
+      id: idUser
+    }
+  })
+  .then((user) => {
+    if(user === undefined){
+      return;
+    }
+  
+    if(newFirstname === undefined) {
+      newFirstname = user.firstname;
+    }
+  
+    if(newLastname === undefined) {
+      newLastname = user.lastname;
+    }
+  
+    if(newPhone === undefined) {
+      newPhone = user.phone;
+    }
+  
+    if(newEmail === undefined) {
+      newEmail = user.email;
+    }
+    if(newPhoto === undefined) {
+      newPhoto = user.photo;
+    }
+    if(newAddress === undefined) {
+      newAddress = user.address;
+    }
+    if(newPassword === undefined) {
+      newPassword = user.password;
+    }
+    if(newId_categ === undefined) {
+      newId_categ = user.category_id;
+    }
+  
+    return user.updateAttributes({
+      firstname: newFirstname,
+      lastname: newLastname,
+      phone: newPhone,
+      email: newEmail,
+      photo: newPhoto,
+      address: newAddress,
+      password: newPassword,
+      category_id: newId_categ
+    });
+
+  })
+  .catch((err)=>{
+    console.error(err);
+  });
+  
+};
+
+
+PropositionController.getPropositionById = function(idUser){
+  return User.find({
+    where: {
+      id: idUser
+    }
+  })
+  .then((user) => {
+    if(user) {
+      console.log('User found.');
+      return user;
+    }
+    return null;
+  })
+  .catch((error) => {
+    console.error(err);
+  });
+};
+
+/*
+UserController.getAllUser = function(){
+  return User.findAll()
+  .catch((err) => {
+    console.error(err);
+  });
+};
+
+
+UserController.login = function(email, password){
+  return User.find({
+    where : {
+      email : email,
+      password : password
+    }
+  })
+  .then((user)=>{
+    if(user){
+      return user;
+    }
+    else{
+      return null;
+    }
+  })
+};
+*/
+module.exports = UserController;
---
+++ routes/proposition.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('../controllers');
+const UserController = controllers.UserController;
+//const jwt = require('jsonwebtoken');
+
+const userRouter = express.Router();
+userRouter.use(bodyParser.json());
+
+userRouter.post('/', function(req, res) {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const photo = req.body.photo;
+  const address = req.body.address;
+  const password = req.body.password;
+  
+  let id_categ = parseInt(req.body.id_category);
+
+  if(isNaN(id_categ)) {
+    id_categ = undefined;
+  }
+
+  const user =  UserController.addUser(firstname, lastname, phone, email, photo, address, password, id_categ)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
+userRouter.delete('/delete/:idUser' , function(req,res){
+  const idUser = req.params.idUser;
+
+  if(idUser === undefined){
+    res.status(500).end();
+    return;
+  }
+  UserController.deleteUser(idUser)
+  .then((user) => {
+    res.status(201).json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+userRouter.put('/update' , function(req,res){
+  const idUser = req.body.id;
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const photo = req.body.photo;
+  const address = req.body.address;
+  const password = req.body.password;
+  let id_categ = parseInt(req.body.id_category);
+
+  if(isNaN(id_categ)) {
+    id_categ = undefined;
+  }
+
+  UserController.updateUser(idUser, firstname, lastname, phone, email, photo, address, password, id_categ)
+  .then((user)=>{
+    console.log("User was successfully updated.");
+    res.status(200).json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+userRouter.get('/getById/:id' , function(req,res){
+  UserController.getUserById(req.params.id)
+  .then((user) => {
+    if(user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+/*
+userRouter.post('/login', function(req, res){
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = UserController.login(email, password)
+  .then((user) => {
+    if(user == null){
+      res.send('Accès refusé').end();
+      return;
+    }
+
+    jwt.sign({user}, 'secretkey', {expiresIn: '1h'}, (err, token) =>{
+      res.json({
+        token
+      });
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+userRouter.get('/allUser', function(req,res){
+  UserController.getAllUser()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+userRouter.get('/getUserById/:id' , function(req,res){
+  UserController.getUserById(req.params.id)
+  .then((user) => {
+    res.status(201).json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).end();
+  })
+});
+
+
+
+userRouter.put('/updateUser' , function(req,res){
+  const token = req.headers["authorization"];
+  jwt.verify(token, 'secretkey', (err) =>{
+  if(err){
+    res.status(403).end('Accès refusé');
+      return;
+  }
+  else{
+    const idUser = req.body.idUser;
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    UserController.updateUser(idUser, username, password, email)
+    .then(()=>{
+      console.log("L'utilisateur à été mis à jour");
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }});
+});
+*/
+
+
+module.exports = userRouter;
